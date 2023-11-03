7dd5ebdaf3a63e9b26ed2d4c19319b6250c1223b
Chris Nesbitt-Smith <chris@cns.me.uk>
2023-11-03 23:45:07 +0000

    ignore pwsp css selectors

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,19 @@ const autoprefixer = require('autoprefixer')
 const purgecss = require('@fullhuman/postcss-purgecss')
 
 const plugins = [
-                  autoprefixer,
-                  purgecss({
-                    content: ['./dist/**/*.html'],
-                    safelist: ['active', 'dark-theme', 'light-theme', 'no-webp', 'webp']
-                  })
-                ]
+  autoprefixer,
+  purgecss({
+    content: ["./dist/**/*.html"],
+    safelist: [
+      "active",
+      "dark-theme",
+      "light-theme",
+      "no-webp",
+      "webp",
+      /^pswp/,
+    ],
+  }),
+];
 
 module.exports = {
   plugins
